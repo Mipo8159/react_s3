596edbec0d91f1014a37e6bf7e2b3ac059d3b2c8
Giga_sft <pumpumchiik@gmail.com>
2022-10-13 16:25:35 +0400

    oke

--- client/src/components/S3.tsx
+++ client/src/components/S3.tsx
@@ -1,18 +1,49 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect, FormEvent } from "react";
+import { $api } from "../http";
 
 const S3: React.FC = () => {
-  const [file, setFIle] = useState<File>();
+  const [file, setFile] = useState<File>();
+  const [preview, setPreview] = useState<string | null>(null);
+  useEffect(() => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setPreview(null);
+    }
+  }, [file]);
 
-  const submit = async () => {
-    const { data } = await axios.get("/files/signed-url");
+  const submit = async (e: FormEvent) => {
+    e.preventDefault();
+    const { data } = await $api.get("/files/signed-url");
+    const { url } = data.data.signature;
 
-    await axios.put(data.url, file, {
+    await axios.put(url, file, {
       headers: { "Content-Type": file?.type },
     });
   };
 
-  return <div>S3</div>;
+  return (
+    <div className="w-[500px] mx-auto my-10">
+      <form className="flex flex-col bg-gray-50" onSubmit={submit}>
+        <div className="border min-h-[400px] mb-3 flex justify-center items-center">
+          {preview ? <img src={preview} alt="preview" /> : <h5>No image</h5>}
+        </div>
+
+        <input type="file" onChange={(e) => setFile(e.target.files![0])} />
+        <button
+          type="submit"
+          className="mt-3 py-2 bg-black text-white uppercase"
+        >
+          submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default S3;
